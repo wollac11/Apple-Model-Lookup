c83a46120c2f7b7607325076f7b6fcf25e1c5ee7
Charlie Callow <wollac11@gmail.com>
2017-06-21 16:39:09 +0100

    Added serial extraction from Mac

--- lookup.sh
+++ lookup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-# Request serial from user
-echo -n "Please enter Apple serial number: "
-read serial
+# Check if running on target machine
+read -r -p "Are we running on device in question? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+		# Obtain serial from system
+		serial=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
+    ;;
+    *)
+		# Request serial from user
+		echo -n "Please enter Apple serial number: "
+		read serial
+    ;;
+esac
 
 # Remove first 8 chars
 ser_end=$(echo "${serial}" | cut -c 9-)
